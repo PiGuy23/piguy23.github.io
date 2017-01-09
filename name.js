856269f244ddc43804421f5a27ac1d3ef78be644
PiGuy23 <apadilla2222@gmail.com>
2017-01-09 10:06:30 -0700

    Pic W Ran

--- name.js
+++ name.js
@@ -4,8 +4,13 @@ var ran = Math.floor(Math.random() * 15);
 
 var ranc = Math.floor(Math.random() * 5);
 
+var ranw = Math.floor(Math.random() * 5);
+
 document.getElementById("name").innerHTML = names[ran];
 
 var colors = ["#36ed25", "#ed2525", "#2af9f9", "#f92ae1", "#fcb819"];
 
-document.getElementById("name").style.color = colors[ranc];
+
+var size = ["500px", "200px", "450px", "600px", "550px"];
+
+document.getElementById("pic").style.width = size[ranw];
