70787ee2bf8b8b350e8cd0bba7bbd4914769f4f9
PiGuy23 <apadilla2222@gmail.com>
2017-01-09 09:41:18 -0700

    yYUsd

--- name.js
+++ name.js
@@ -1,6 +1,6 @@
-var names = ['Jim', 'Bob', 'Frank'];
+var names = ['Jim', 'Bob', 'Frank', 'Adam', 'Kyle', 'Deveroon', 'Jesus', 'Chris', 'Jessica', 'Nick', 'John', 'Ashton', 'Doug', 'Bret', 'Edward'];
 
-var ran = Math.floor(Math.random() * 3);
+var ran = Math.floor(Math.random() * 15);
 
 document.getElementById("name").innerHTML = names[ran];
 
