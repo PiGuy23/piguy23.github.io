5956b035525dd4b1a8e01707d315c610643113d2
PiGuy23 <apadilla2222@gmail.com>
2017-01-09 10:13:44 -0700

    Moer Size

--- name.js
+++ name.js
@@ -4,7 +4,7 @@ var ran = Math.floor(Math.random() * 15);
 
 var ranc = Math.floor(Math.random() * 5);
 
-var ranw = Math.floor(Math.random() * 5);
+var ranw = Math.floor(Math.random() * 9);
 
 document.getElementById("name").innerHTML = names[ran];
 
@@ -12,6 +12,6 @@ var colors = ["#36ed25", "#ed2525", "#2af9f9", "#f92ae1", "#fcb819"];
 
 document.getElementById("name").style.color = colors[ranc];
 
-var size = ["500px", "200px", "450px", "600px", "550px"];
+var size = ["500px", "200px", "450px", "600px", "550px", "10px", "800px", "4000px", "18px"];
 
 document.getElementById("pic").style.width = size[ranw];
